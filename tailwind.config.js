78b911a9cc956c2d749de35163671e12e4b024a1
sunmou886 <44077863+sunmou886@users.noreply.github.com>
2025-08-25 16:06:10 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 export default {
-content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-theme: { extend: {} },
-plugins: [],
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: { extend: {} },
+  plugins: []
 }
